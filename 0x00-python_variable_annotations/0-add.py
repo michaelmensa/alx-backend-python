e7ab0e3530570a39d7310e2148cb33e101da9408
michaelmensa <michaelmensa@live.com>
2023-12-07 13:17:58 +0000

    task0: add-py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module: 0 basic annotations - add
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    function add that returns the sum of a and b as params
+    params:
+    a (float)
+    b (float)
+    return a + b (float)
+    '''
+    return a + b
