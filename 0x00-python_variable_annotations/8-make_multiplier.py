bdd18a1cc51cddabe2a71583825ad467508caa66
michaelmensa <michaelmensa@live.com>
2023-12-07 15:15:30 +0000

    task8: make_multiplier using callable: tedious

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Module 8: type-annotated func make_multiplier that takes a multiplier
+float as param and returns a function that multiplies a float by multiplier
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[x: float]: callable]:
+    '''
+    function that returns a function that multiplies a float by multiplier
+    '''
+    def multiplier_fuction(x: float) -> float:
+        '''
+        function that is going to be called by the make_multiplier funciton
+        '''
+        return x * multiplier
+    return multiplier_function
