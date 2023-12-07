2fcba7321901ddaf49adb21a3cfc44af8051a051
michaelmensa <michaelmensa@live.com>
2023-12-07 13:52:47 +0000

    task2: floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Module 2: type-annotated func floor that takes float n as param and returns
+the floor of the float.
+'''
+
+
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    function returns the floor of a float which is an int
+    '''
+    return math.floor(n)
