5e4ef6c7093bcaf749e315cbbf5f9c1ef5b43e81
michaelmensa <michaelmensa@live.com>
2023-12-07 14:01:40 +0000

    task3: to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Module 3: type-annotated func to_str that takes a float n as param
+and returns the string representation of the float
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    function takes a float and returns a string representation of the
+    float
+    '''
+    return str(n)
