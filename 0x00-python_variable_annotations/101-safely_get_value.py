4378b75c590bb23bc0115d99ba3e1420dd5aa48e
michaelmensa <michaelmensa@live.com>
2023-12-07 16:13:22 +0000

    adv.task2: looking into TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module 11: looking into TypeVar, still duck typing
+'''
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    '''
+    Given the params and return values, add type annotations to this
+    function
+    '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
