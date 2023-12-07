0e1b5b990d1644deaa58e389d9d95b21947185cc
michaelmensa <michaelmensa@live.com>
2023-12-07 13:40:35 +0000

    task1: concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Module 1: type-annotated func concat that takes str1 and str2 as params
+and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' function returns a concatenated string '''
+    return str1 + str2
